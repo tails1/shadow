38fdc70d38099d80ec844da7d8a3b79fecb56cec
jjolano <jjolano@me.com>
2019-08-09 00:10:30 -0400

    create orig-fs map

--- ShadowHelper/main.m
+++ ShadowHelper/main.m
@@ -7,6 +7,11 @@
 #import <Foundation/Foundation.h>
 
 int mount_apfs_snapshot(char *snapshot, char *volume, char *directory) {
+	// Check if already mounted.
+	if([[NSFileManager defaultManager] fileExistsAtPath:@"/var/MobileSoftwareUpdate/mnt1/.file"]) {
+		return 0;
+	}
+
 	pid_t pid;
 	char *argv[] = {"mount_apfs", "-s", snapshot, volume, directory, NULL};
 	int ret;
@@ -31,10 +36,26 @@ int main(int argc, char *argv[], char *envp[]) {
 		}
 	}
 
+	// Mount orig-fs.
 	if(mount_apfs_snapshot("orig-fs", "/", "/var/MobileSoftwareUpdate/mnt1") != 0) {
 		printf("error: failed to mount orig-fs.\n");
 		return -1;
 	}
 
+	// Enumerate snapshot files and compile into a plist.
+	NSMutableDictionary *plist = [NSMutableDictionary new];
+	NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:@"/var/MobileSoftwareUpdate/mnt1"];
+
+	NSString *file;
+	BOOL *isDir;
+
+	while((file = [dirEnum nextObject])) {
+		if([[NSFileManager defaultManager] fileExistsAtPath:file isDirectory:&isDir]) {
+			plist[file] = isDir;
+		}
+	}
+
+	// Write plist to Shadow data directory.
+	[plist writeToFile:@"/Library/Shadow/orig-fs.map.plist" atomically:YES];
 	return 0;
 }
